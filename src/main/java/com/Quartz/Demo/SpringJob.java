f1cd8c65104167bb99650222f689fd57b0ee907b
Mr-hang <1083594261@qq.com>
2017-08-10 22:00:14 +0800

    SpringTask

---
+++ src/main/java/com/Quartz/Demo/SpringJob.java
@@ -0,0 +1,15 @@
+package com.Quartz.Demo;
+
+/**
+ * Spring的定时任务支持
+ * @author Mr-hang
+ *
+ */
+public class SpringJob {
+	/** 
+	* 要调度的具体任务 
+	*/  
+	public  void run(){  
+	  System.out.println("Spring大法好…");  
+	}  
+}
